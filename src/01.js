010bb6206d7eb9be8aad82ce67f368752e7d68c0
Felipe Chernicharo <string7dev@gmail.com>
2020-09-12 18:10:00 -0300

    01

---
+++ src/01.js
@@ -0,0 +1,37 @@
+import { interval, Observable, pipe } from 'rxjs';
+import { debounce, map, take } from 'rxjs/operators';
+
+// criar observable
+const obs = new Observable((observer) => {
+	console.log('iniciando Observable')
+	observer.next(1)
+	observer.next(2)
+	observer.next(3)
+})
+
+// criar promise
+const promise = new Promise((resolve) => {
+	console.log('iniciando Promise')
+	resolve(1)
+	resolve(2) // uma promise só resolve uma única vez, portanto essa linha com o número 2 nunca é executada
+})
+
+promise.then(data => console.log('Promise ->'  + data))
+setTimeout(() => {
+	promise.then(data => console.log('Promise ->'  + data))
+}, 6000)
+
+// observables só são executadas a partir do momento em que tem alguém escutando
+setTimeout(() => {
+	obs.subscribe(data => {
+		console.log('Observable ->' + data)
+	});
+}, 2000)
+
+
+setTimeout(() => {
+	obs.subscribe(data => {
+		console.log('2a subscription ->' + data)
+	});
+}, 4000)
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	mode: "development",
-	entry: './src/main.js',
+	entry: './src/01.js',
 	devServer: {
     contentBase: './dist',
   },
