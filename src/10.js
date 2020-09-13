b986cdc9b1f917c93afc9b003abe004536b8525f
Felipe Chernicharo <string7dev@gmail.com>
2020-09-13 00:41:48 -0300

    subject

--- src/10.js
+++ src/10.js
@@ -1,33 +1,36 @@
-import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
 
 // stream -> fluxo de dados
 // observable é um stream
 // pra observar o stream, inscreva-se
 
 // subjects são observables especiais
+// ao mesmo tempo que são observables são também observers
+// portanto podemos nos inscrever e depois emitir .next() externamente
 
-const myObservable = new Observable((observer) => {
-	let num = 1;
-	console.log('Iniciando Observable');
-	const interval = setInterval(() => observer.next(num++), 1000);
-	setTimeout(() => observer.error(new Error('erro!')), 3000);
+const subject = new Subject();
 
-	setTimeout(() => observer.complete(), 5000);
-	return () => clearInterval(interval);
-});
-
-const subscription = myObservable.subscribe(
-	(data) => console.log('Observer 1 ' + data),
+let i = 0;
+const subscription = subject.subscribe(
+	(data) => console.log('Observer 1: ' + data),
 	(err) => console.log(err),
 	() => console.log('complete')
 );
 
-const subscription2 = myObservable.subscribe({
-	next: (data) => console.log('Observer 2 ' + data),
+const subscription2 = subject.subscribe({
+	next: (data) => console.log('Observer 2: ' + data),
 	error: (err) => console.log(err),
 	complete: () => console.log('complete'),
 });
 
+const interval = setInterval(() => {
+	subject.next(i++);
+	console.log(i);
+}, 1000);
+
 subscription.add(subscription2); // junta a subscription 1 com a 2 pra dar o unsubscribe junto
 
-setTimeout(() => subscription.unsubscribe(), 6000);
+setTimeout(() => {
+	subscription.unsubscribe();
+	clearInterval(interval);
+}, 6000);
