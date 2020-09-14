1106eb4239053f724ce3f4222a6266d6fc855bc2
Felipe Chernicharo <string7dev@gmail.com>
2020-09-13 23:58:20 -0300

    mergeMap, concatMap, switchMap, exhaustMap

--- src/30.js
+++ src/30.js
@@ -1,5 +1,14 @@
-import { of } from 'rxjs';
-import { map, mergeMap, tap } from 'rxjs/operators';
+import { concat, merge, of } from 'rxjs';
+import {
+	concatMap,
+	map,
+	mergeMap,
+	pluck,
+	switchMap,
+	exhaustMap,
+	tap,
+	combineAll,
+} from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 
 const api = (response, delay) =>
@@ -19,10 +28,32 @@ const i = api({ data: 'I' }, 4500);
 const j = api({ data: 'J' }, 5000);
 const k = api({ data: 'J' }, 6000);
 
-of(a, b, c, d, e, f, g, h, i, j, k)
-	.pipe(mergeMap((val) => val))
+// of(a, b, c, d, e, f, g, h, i, j, k);
+merge(a, b, c, d, e, f, g, h, i, j, k)
+	// concat(a, b, c, d, e, f, g, h, i, j, k)
+	// .pipe(map((val) => val))  // apenas retorna todos os observables ao mesmo tempo sem executá-los. retorna Ajax Observable
+	.pipe(
+		// mergeMap((val) => val), //retorna objetos tipo Ajax Response
+		// concatMap((val) => val),
+		// switchMap((val) => val),
+		// exhaustMap((val) => val),
+		// mergeMap((val) => val, 2),
+		pluck('response', 'data'), // acessa o response.data do objeto AjaxResponse. // printando 'A', 'B', 'C' ...
+		combineAll()
+	)
+
 	.subscribe(
 		(x) => console.log(x),
 		(error) => console.log(error),
 		() => console.log('complete')
 	);
+
+// MergeMap => executa todas as requisições ao mesmo tempo na medida do possível. Conforme as primeiras vão concluindo ele vai disparando as que faltam
+// ConcatMap => envia uma requisição por vez. Só envia a seguinte quando a primeira concluiu
+// SwitchMap => chama todas mas não espera: se tem mais pra chamar, ele cancela a atual e chama a próxima, cancela e chama a próxima, cancela e chama a próxima, até chegar na última, que é a única que ele deixa completar
+// exhaustMap => ao contrário do switchMap, essa mantém apenas a primeira requisição e não dispara as demais. Enquanto a atual estiver executando, todas as demais são ignoradas.
+
+// * exhaustMap é útil pra formulários, pra evitar que o usuário mande o mesmo form várias vezes se ele socar o botão várias vezes
+// * mergeMap pode ter seu máximo de paralelismo limitado. =>  mergeMap((val) => val, 3)  <== só dispara um máximo de 3 reqs simultânes
+
+// vai cancelando a atual e chama a próxima
